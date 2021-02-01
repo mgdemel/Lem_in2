440a53fa468f143846bfec4bdd7a3dc1e542a54c
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2021-02-01 08:20:28 +0200

    not working but switching to my imac to continue

--- srcs/store_data.c
+++ srcs/store_data.c
@@ -13,28 +13,19 @@ void	free_array(char **array)
 	free(array);
 }
 
-void 	get_room_data(char *line, t_room *room, int fd)
+void 	get_room_data(char *line, t_room *room, int roomtype)
 {
 	int		i;
 	char	**coord;
 	i = 0;
 
-	if (ft_strstr((char*)line, "##start")) //labels room based on type
-	{
-		room->roomtype = 1;
-		ft_strdel(&line);
-		get_next_line(fd, &line);
-	}
-	else if (ft_strstr((char*)line, "##end"))
-	{
-		room->roomtype = 3;
-		ft_strdel(&line);
-		get_next_line(fd, &line);
-	}
-	else
-		room->roomtype = 2;
+	ft_putstr("In get room data: ");
+	ft_putstr(line);
+	ft_putchar('\n');
+	room->roomtype = roomtype;
 	if (!(room->name = (char*)malloc(sizeof(char) * ((int)ft_strlen(line) + 1))))
 		ft_putstr("ERROR. COULD NOT MALLOC ROOM NAME\n");
+	line[(int)ft_strlen(line) + 1] = '\0';
 	while (line[i] != ' ') //stores the name of the room
 	{
 		room->name[i] = line[i];
@@ -50,14 +41,12 @@ void 	get_room_data(char *line, t_room *room, int fd)
 
 t_room		*get_room(char *line, t_lem *lem, t_room *room, int fd)
 {
-	ft_putstr("checking for head node\n");
 	t_room *new_room;
 
 	new_room = initialize_room();
 	room->next = new_room;
 	if (room->first == 0)
 	{
-		ft_putstr("here\n");
 		lem->temp = room;
 		room->prev = lem->temp;
 		get_room_data(line, new_room, fd);
@@ -69,25 +58,44 @@ t_room		*get_room(char *line, t_lem *lem, t_room *room, int fd)
 int				store_data(char *line, t_lem *lem, t_room *room, int fd)
 {
 	int i;
+	int roomtype;
 
 	i = 0;
 	get_next_line(fd, &line);
 	lem->ants = ft_atoi(line);
 	ft_strdel(&line);
-	if (!(lem->tunnels = (char**)malloc(sizeof(char*) * (lem->nbr_tunnels + 1)))) //allocates tunnels array
+	if (!(lem->tunnels = (char**)malloc(sizeof(char*) * (lem->nbr_tunnels)))) //allocates tunnels array
 		return (1);
 	while (get_next_line(fd, &line) > 0)
 	{
 	//	if (line[0] == '#' && line[1] != '#') nevermind doesn't work, fix this lukas!
-	//	{
-			if (!(ft_strstr(line, "-")))
-			room = get_room(line, lem, room, fd);
+	//	{	
+			ft_putstr("In GNL while loop: ");
+			ft_putstr(line);
+			ft_putchar('\n');
+			roomtype = 2;
+			if (!(ft_strstr(line, "-") && ft_strstr((char*)line, "##")))
+			{
+				if (ft_strstr((char*)line, "##start"))
+				{
+					roomtype = 1;
+					ft_strdel(&line);
+					get_next_line(fd, &line);
+				}
+				else if (ft_strstr((char*)line, "##end"))
+				{
+					roomtype = 3;
+					ft_strdel(&line);
+					get_next_line(fd, &line);
+				}
+			}
+			if (!(ft_strstr(line, "-") && (!(ft_strstr((char*)line, "##")))))
+				room = get_room(line, lem, room, roomtype);
 			else
 			{
 				lem->tunnels[i] = ft_strdup(line);
 				i++;
 			}
-			ft_strdel(&line);
 	//	}
 	}
 	return (0);
